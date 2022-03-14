6553812273f6dde52e91199ccb8746f66a7514d4
Joe Valookaran <joevalookaran@hotmail.com>
2022-03-14 17:24:26 +0000

    created an about page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     post   :add_item
     post   :remove_item
   end
-
+  get '/about', to: 'about#index'
   resources :orders, only: [:create, :show]
 
   namespace :admin do
